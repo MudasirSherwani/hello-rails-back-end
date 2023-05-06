4ef14aacd5827769178598bbeb8d6f765e948b99
MudasirSherwani <mudasir9757@gmail.com>
2023-05-06 11:58:47 +0500

    Add greeting model and controller

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,10 @@
+class GreetingsController < ApplicationController
+    def index
+        @greeting = Greeting.order('RANDOM()').first
+        if @greeting
+          render json: { greeting: @greeting }, status: :ok
+        else
+          render json: { success: false, errors: 'Oops, Something went wrong!' }, status: :unprocessable_entity
+        end
+      end
+end
